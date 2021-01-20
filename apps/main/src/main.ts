6fc10c8fc1a4c86e5b10a161d3ff2c4b35c67feb
iamvasiliev143 <iamvasiliev143@gmail.com>
2021-01-20 14:35:19 +0200

    Add class-validator

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-
-import config from './config/config';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
+import config from './config/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.APP_PORT);
 }
 bootstrap();
