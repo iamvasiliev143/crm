963966611b235862f21d27e56372fa2db6609d5c
iamvasiliev143 <mikhail@kenmoredeisgn.com>
2021-04-02 11:49:47 +0300

    fix password generator

--- apps/core/src/shared/helpers/make-password.ts
+++ apps/core/src/shared/helpers/make-password.ts
@@ -1,12 +1,26 @@
-import generator from 'generate-password';
+export const PasswordGenerator = function () {
+  const symbols =
+    '!?.,@#$%^&*_+-=<>(){}[]0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export const MakePassword = () =>
-  generator.generate({
-    length: 8,
-    uppercase: true,
-    lowercase: true,
-    numbers: true,
-    symbols: true,
-    exclude: '\'".,|',
-    strict: true,
-  });
+  let password = '';
+  for (let i = 0; i < 8; i++) {
+    password += symbols.charAt(Math.floor(Math.random() * 62));
+  }
+
+  return password;
+};
+
+export const MakePassword = () => {
+  let password = PasswordGenerator();
+
+  while (
+    !/[A-Z]/.test(password) ||
+    !/[0-9]/.test(password) ||
+    !/[a-z]/.test(password) ||
+    !/[!?.,@#$%^&*_+-=<>(){}[]]/.test(password)
+  ) {
+    password = PasswordGenerator();
+  }
+
+  return password;
+};
