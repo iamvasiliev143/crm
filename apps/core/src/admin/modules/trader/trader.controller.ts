0ef35d118ab0058842bc8c66c30d75160b6c78b2
iamvasiliev143 <mikhail@kenmoredeisgn.com>
2021-04-06 18:57:55 +0300

    create Trader module

--- apps/core/src/admin/modules/trader/trader.controller.ts
+++ apps/core/src/admin/modules/trader/trader.controller.ts
@@ -1,8 +1,20 @@
-import { Logger } from '@nestjs/common';
+import { Logger, Get } from '@nestjs/common';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+
 import { TraderService } from '@core/admin/services';
 
 export class TraderController {
   public readonly logger = new Logger(TraderController.name);
 
   constructor(protected readonly traderService: TraderService) {}
+
+  @Get('/')
+  @ApiOperation({
+    summary: 'Get traders',
+  })
+  @ApiQuery({ name: 'pageIndex', required: false })
+  @ApiQuery({ name: 'pageSize', required: false })
+  async getAllTraders() {
+    return await this.traderService.getAllTraders();
+  }
 }
--- apps/core/src/admin/modules/trader/trader.service.ts
+++ apps/core/src/admin/modules/trader/trader.service.ts
@@ -2,10 +2,40 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Trader as CoreTrader } from '@core/shared/entities';
+import { ToLocalDateTime } from '@core/shared/helpers';
+
+const publicValues: any = [
+  'id',
+  'firstName',
+  'lastName',
+  'email',
+  'phone',
+  'messengerType',
+  'messengerAccount',
+  'country',
+  'province',
+  'city',
+  'zip',
+  'leverage',
+  'timezone',
+  'createdAt',
+];
 
 export class TraderService {
   constructor(
     @InjectRepository(CoreTrader)
     protected readonly traderRepo: Repository<CoreTrader>,
   ) {}
+
+  async getAllTraders() {
+    const traders = await this.traderRepo.find({
+      relations: ['tasks'],
+      select: publicValues,
+    });
+
+    return traders.map(
+      (trader) =>
+        (trader.createdAt = ToLocalDateTime(trader.timezone, trader.createdAt)),
+    );
+  }
 }
